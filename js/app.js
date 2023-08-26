28ddfc485fdd9dc0e0a8979433267cd29cb56acc
EJ Milord <jmilord7@gmail.com>
2023-08-26 09:21:42 -0400

    Disabled Horizontal Scroll

--- js/app.js
+++ js/app.js
@@ -195,6 +195,7 @@ function setGreeting(hours) {
 
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center"
   }
 }
 
